469cef428021689cf7a8fe969f5f7080c45e9c65
Jeff <jolson615@gmail.com>
2019-06-23 14:59:53 -0400

    Update challenge.py

--- challenge.py
+++ challenge.py
@@ -14,42 +14,42 @@ import database
 
 
 
-# 2. For each person on this list, print out a descriptor of where they live in the form "____ lives in _____."
+# 3. For each person on this list, print out a descriptor of where they live in the form "____ lives in _____."
 
 
 
 
-# 3. Print out each person's nickname. Write some control flow that determines whether / how to respond if someone didn't provide a nickname.
+# 4. Print out each person's nickname. Write some control flow that determines whether / how to respond if someone didn't provide a nickname.
 
 
 
 
-# 4. Find the average of the respondents' ages.
+# 5. Find the average of the respondents' ages.
 
 
 
 
-# 5. Print out the percentage of respondents who have brown hair.
+# 6. Print out the percentage of respondents who have brown hair.
 
 
 
 
-# 6. Create two new lists - one for programmers, and one for non programmers.
+# 7. Create two new lists - one for programmers, and one for non programmers.
 #    Sort all the people in the main list into these two more specific lists.
 
 
 
 
-# 7. Iterate over the list in order to figure out how many respondents listed "The Office" as their one of their favorite shows.
+# 8. Iterate over the list in order to figure out how many respondents listed "The Office" as their one of their favorite shows.
 
 
 
 
-# 8. Print out a list of all the bands that are liked by at least two people.
+# 9. Print out a list of all the bands that are liked by at least two people.
 #    Pro-tip: some respondents capitalized band names and other respondents did not, so for example, if Panic at the Disco isn't listed, your code isn't accurately reporting your results.
 
 
 
 
 
-# 9. Identify anyone on the list who has no common interests (bands or shows) with anyone else. Print their names.
+# 10. Identify anyone on the list who has no common interests (bands or shows) with anyone else. Print their names.
